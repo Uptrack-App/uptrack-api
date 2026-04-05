814fe760dcd189c4bb9cc714d4659418a9eb490b
hoang <giahoangth@gmail.com>
2026-04-05 10:15:14 +0700

    Update Nix deps hash for Gun dependency

--- infra/nixos/modules/packages/uptrack-app.nix
+++ infra/nixos/modules/packages/uptrack-app.nix
@@ -20,7 +20,7 @@ beamPackages.mixRelease {
     pname = "uptrack-deps";
     version = "0.1.0";
     inherit src;
-    sha256 = "sha256-T6u4J9qXboEEi3KU9yxSQYQTpWWPhLdsLOTFc2qM0gw=";
+    sha256 = "sha256-DfKAWHcPAplFCG7d9l4MgPIGaTAZjvsmnNg2sDFu5+c=";
   };
 
   # Dependencies needed for compilation
